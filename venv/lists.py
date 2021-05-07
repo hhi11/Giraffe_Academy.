889044527fde48a8079daba7f184dba4b2b2b9f0
henryh <hhill@outlook.com>
2021-05-07 15:49:28 -0600

    update 1

--- venv/lists.py
+++ venv/lists.py
@@ -1,2 +1,23 @@
-friends = ["Harry", "Mary", "Janet"]
+lucky_numbers = [45, 56,98,34,23]
+friends = ["Harry", "Mary", "Janet", "Bob", "Halbert"]
+friends.extend(lucky_numbers)
 print(friends)
+
+lucky_numbers = [45, 56,98,34,23]
+friends = ["Harry", "Mary", "Janet", "Bob", "Halbert"]
+friends.append("creed")
+print(friends)
+
+
+lucky_numbers = [45, 56,98,34,23]
+friends = ["Harry", "Mary", "Janet", "Bob", "Halbert"]
+friends.insert(2,"Charlie")
+print(friends)
+
+lucky_numbers = [45, 56,98,34,23]
+friends = ["Harry", "Mary", "Janet", "Bob", "Halbert"]
+friends.remove("Mary")
+print(friends)
+
+
+
